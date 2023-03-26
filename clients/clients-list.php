9b410e147f07899a06f8322b500386c13e179744
m4hfuj <mahfujmirza273@gmail.com>
2023-03-27 04:47:42 +0600

    up

--- clients/clients-list.php
+++ clients/clients-list.php
@@ -6,13 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="reg-client.css"> -->
-  <title>List of CLients</title>
+  <title> List of CLients </title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Deposit System</a>
+      <a class="navbar-brand" href="#"> Deposit System </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,17 +20,17 @@
         <ul class="navbar-nav">
 
           <li class="nav-item">
-            <a class="nav-link" href="/safety-deposit-php">Home</a>
+            <a class="nav-link" href="/safety-deposit-php"> Home </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="reg-client.html">Register new Client</a>
+            <a class="nav-link" href="reg-client.html"> Register new Client </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="clients/clients-list.php">List of Clients</a>
+            <a class="nav-link active" href="clients/clients-list.php"> List of Clients </a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="bill-creation/boxes.php">Register Box</a>
+            <a class="nav-link" href="bill-creation/boxes.php"> Register Box </a>
           </li>
 
         </ul>
